af9ce7c64f5c76c81c889dcad9c343ebece11ac0
carlos <cpm1807@gmail.com>
2022-03-09 16:21:27 +0100

    deployment ready

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,6 @@
-npm run build
 cd dist
+git init
 git add -A
-git commit -m 'New'
-git push -f git@github.com:chumpbo/metabalance-frontend.git master:gh-pages
+git commit -m 'New' 
+git push -f git@github.com:chumpbo/metabalance-frontend.git master:gh-pages 
 cd ..
