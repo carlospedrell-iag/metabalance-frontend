414faab99b060b35b442066669d2503a3294c90d
carlos <cpm1807@gmail.com>
2022-04-20 12:15:52 +0200

    Diet is Clickable

--- src/views/ModifyDietView.vue
+++ src/views/ModifyDietView.vue
@@ -6,7 +6,7 @@
 
     <h2>Monday</h2>
 
-    <div class="meal">
+    <div class="meal" @click="editDish('monday_breakfast')">
       <p class="meal-title">Breakfast</p>
       <ul>
         <li v-for="(dish) in schedule.monday.breakfast.dishes" :key="dish">      
@@ -15,7 +15,7 @@
       </ul>  
     </div>
 
-    <div class="meal">    
+    <div class="meal" @click="editDish('monday_lunch')">    
       <p class="meal-title">Lunch</p>
       <ul>
         <li v-for="(dish) in schedule.monday.lunch.dishes" :key="dish">      
@@ -24,7 +24,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('monday_snack')">  
       <p class="meal-title">Snack</p>
       <ul>
         <li v-for="(dish) in schedule.monday.snack.dishes" :key="dish">      
@@ -33,7 +33,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('monday_dinner')">  
       <p class="meal-title">Dinner</p>
       <ul>
         <li v-for="(dish) in schedule.monday.dinner.dishes" :key="dish">      
@@ -49,7 +49,7 @@
 
     <h2>Tuesday</h2>
 
-    <div class="meal">
+    <div class="meal" @click="editDish('tuesday_breakfast')">
       <p class="meal-title">Breakfast</p>
       <ul>
         <li v-for="(dish) in schedule.tuesday.breakfast.dishes" :key="dish">      
@@ -58,7 +58,7 @@
       </ul>  
     </div>
 
-    <div class="meal">    
+    <div class="meal" @click="editDish('tuesday_lunch')">    
       <p class="meal-title">Lunch</p>
       <ul>
         <li v-for="(dish) in schedule.tuesday.lunch.dishes" :key="dish">      
@@ -67,7 +67,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('tuesday_snack')">  
       <p class="meal-title">Snack</p>
       <ul>
         <li v-for="(dish) in schedule.tuesday.snack.dishes" :key="dish">      
@@ -76,7 +76,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('tuesday_dinner')">  
       <p class="meal-title">Dinner</p>
       <ul>
         <li v-for="(dish) in schedule.tuesday.dinner.dishes" :key="dish">      
@@ -92,7 +92,7 @@
 
     <h2>Wednesday</h2>
 
-    <div class="meal">
+    <div class="meal" @click="editDish('wednesday_breakfast')">
       <p class="meal-title">Breakfast</p>
       <ul>
         <li v-for="(dish) in schedule.wednesday.breakfast.dishes" :key="dish">      
@@ -101,7 +101,7 @@
       </ul>  
     </div>
 
-    <div class="meal">    
+    <div class="meal" @click="editDish('wednesday_lunch')">    
       <p class="meal-title">Lunch</p>
       <ul>
         <li v-for="(dish) in schedule.wednesday.lunch.dishes" :key="dish">      
@@ -110,7 +110,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('wednesday_snack')">  
       <p class="meal-title">Snack</p>
       <ul>
         <li v-for="(dish) in schedule.wednesday.snack.dishes" :key="dish">      
@@ -119,7 +119,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('wednesday_dinner')">  
       <p class="meal-title">Dinner</p>
       <ul>
         <li v-for="(dish) in schedule.wednesday.dinner.dishes" :key="dish">      
@@ -135,7 +135,7 @@
 
     <h2>Thursday</h2>
 
-    <div class="meal">
+    <div class="meal" @click="editDish('thursday_breakfast')">
       <p class="meal-title">Breakfast</p>
       <ul>
         <li v-for="(dish) in schedule.thursday.breakfast.dishes" :key="dish">      
@@ -144,7 +144,7 @@
       </ul>  
     </div>
 
-    <div class="meal">    
+    <div class="meal" @click="editDish('thursday_lunch')">    
       <p class="meal-title">Lunch</p>
       <ul>
         <li v-for="(dish) in schedule.thursday.lunch.dishes" :key="dish">      
@@ -153,7 +153,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('thursday_snack')">  
       <p class="meal-title">Snack</p>
       <ul>
         <li v-for="(dish) in schedule.thursday.snack.dishes" :key="dish">      
@@ -162,7 +162,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('thursday_dinner')">  
       <p class="meal-title">Dinner</p>
       <ul>
         <li v-for="(dish) in schedule.thursday.dinner.dishes" :key="dish">      
@@ -178,7 +178,7 @@
 
     <h2>Friday</h2>
 
-    <div class="meal">
+    <div class="meal" @click="editDish('friday_breakfast')">
       <p class="meal-title">Breakfast</p>
       <ul>
         <li v-for="(dish) in schedule.friday.breakfast.dishes" :key="dish">      
@@ -187,7 +187,7 @@
       </ul>  
     </div>
 
-    <div class="meal">    
+    <div class="meal" @click="editDish('friday_lunch')">    
       <p class="meal-title">Lunch</p>
       <ul>
         <li v-for="(dish) in schedule.friday.lunch.dishes" :key="dish">      
@@ -196,7 +196,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('friday_snack')">  
       <p class="meal-title">Snack</p>
       <ul>
         <li v-for="(dish) in schedule.friday.snack.dishes" :key="dish">      
@@ -205,7 +205,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('friday_dinner')">  
       <p class="meal-title">Dinner</p>
       <ul>
         <li v-for="(dish) in schedule.friday.dinner.dishes" :key="dish">      
@@ -221,7 +221,7 @@
 
     <h2>Saturday</h2>
 
-    <div class="meal">
+    <div class="meal" @click="editDish('saturday_breakfast')">
       <p class="meal-title">Breakfast</p>
       <ul>
         <li v-for="(dish) in schedule.saturday.breakfast.dishes" :key="dish">      
@@ -230,7 +230,7 @@
       </ul>  
     </div>
 
-    <div class="meal">    
+    <div class="meal" @click="editDish('saturday_lunch')">    
       <p class="meal-title">Lunch</p>
       <ul>
         <li v-for="(dish) in schedule.saturday.lunch.dishes" :key="dish">      
@@ -239,7 +239,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('saturday_snack')">  
       <p class="meal-title">Snack</p>
       <ul>
         <li v-for="(dish) in schedule.saturday.snack.dishes" :key="dish">      
@@ -248,7 +248,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('saturday_dinner')">  
       <p class="meal-title">Dinner</p>
       <ul>
         <li v-for="(dish) in schedule.saturday.dinner.dishes" :key="dish">      
@@ -264,7 +264,7 @@
 
     <h2>Sunday</h2>
 
-    <div class="meal">
+    <div class="meal" @click="editDish('sunday_breakfast')">
       <p class="meal-title">Breakfast</p>
       <ul>
         <li v-for="(dish) in schedule.sunday.breakfast.dishes" :key="dish">      
@@ -273,7 +273,7 @@
       </ul>  
     </div>
 
-    <div class="meal">    
+    <div class="meal" @click="editDish('sunday_lunch')">    
       <p class="meal-title">Lunch</p>
       <ul>
         <li v-for="(dish) in schedule.sunday.lunch.dishes" :key="dish">      
@@ -282,7 +282,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('sunday_snack')">  
       <p class="meal-title">Snack</p>
       <ul>
         <li v-for="(dish) in schedule.sunday.snack.dishes" :key="dish">      
@@ -291,7 +291,7 @@
       </ul>
     </div>
 
-    <div class="meal">  
+    <div class="meal" @click="editDish('sunday_dinner')">  
       <p class="meal-title">Dinner</p>
       <ul>
         <li v-for="(dish) in schedule.sunday.dinner.dishes" :key="dish">      
@@ -357,24 +357,9 @@ export default {
 		
 	},
     methods: {
-        deleteDish(dish_key) {
-            console.log("Deleting, Dish key: " + dish_key);
-			const db = getDatabase();
-			remove(ref(db, 'users/carlos/dishes/' + dish_key));
-
-			const dbRef = ref(db);
-			get(child(dbRef, `users/carlos/dishes`)).then((snapshot) => {
-				if (snapshot.exists()) {
-					console.log(snapshot.val());
-					this.productList = snapshot.val();
-				} else {
-					console.log("No data available");
-					this.productList = []
-				}
-			}).catch((error) => {
-				console.error(error);
-			});
-        }
+      editDish(dish_key) {
+        console.log("Edit Dish:" + dish_key);
+      }
     },
 
 
